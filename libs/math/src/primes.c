eb846dca80e4a6cf702d68fb2c309042aa594f4d
nicolau <marcosnicolau2003@gmail.com>
2025-01-25 00:48:36 -0300

    fix: is_prime solovay_strassen test

--- libs/math/src/primes.c
+++ libs/math/src/primes.c
@@ -8,7 +8,7 @@ void biguint_random_prime(BigUint *a) {
     while (!biguint_is_prime(*a)) {
         biguint_random(a);
     }
-};
+}
 
 // Verifies if a number is prime by dividing it by the first 1000 primes
 // If it passes the initial test, then we run a more strong and probable primality test
@@ -42,12 +42,11 @@ int biguint_is_prime(BigUint a) {
 //
 // If p is prime, then the relation always holds
 // If p is composite, there is a 1/2 chance for the relation to be true.
-// so if we compute 100 numbers between {1...,n-1} the probability of it being a comprime
+// so if we compute 100 numbers between {1...,p-1} the probability of it being a comprime
 // is reduced to the point were it is negligible (1 in (1/2)^100).
 //
 // @reference
 //    - https://en.wikipedia.org/wiki/Solovay%E2%80%93Strassen_primality_test
-//    - https://en.wikipedia.org/wiki/Legendre_symbol
 //    - https://web.archive.org/web/20230127011251/http://people.csail.mit.edu/rivest/Rsapaper.pdf (page 9)
 int biguint_is_prime_solovay_strassen(BigUint p) {
     BigUint a = biguint_new_heap(p.size);
@@ -59,33 +58,49 @@ int biguint_is_prime_solovay_strassen(BigUint p) {
     biguint_sub(&exponent, num);
     biguint_from_u64(2, &num);
     biguint_div(exponent, num, &exponent);
+    int p_bits = biguint_bits(p);
 
-    for (int i = 0; i < 100; i++) {
-        biguint_random(&a);
+    BigUint p_minus_one = biguint_new_heap(p.size);
+    biguint_from_u64(1, &num);
+    biguint_cpy(&p_minus_one, p);
+    biguint_sub(&p_minus_one, num);
+
+    for (int i = 0; i < SOLOVAY_STRASSEN_TEST_SAMPLES; i++) {
+        biguint_random_with_max_bits(&a, p_bits);
         biguint_mod(a, p, &rem);
         // check that a is not divisible by p and a < p
-        if (biguint_is_zero(rem) && biguint_cmp(a, p) < 0) {
+        if (biguint_is_zero(rem) || biguint_cmp(a, p) > 0) {
             // don't count the iteration
             i--;
             continue;
         }
 
+        // since we check that gcd(a, p) == 1
+        // j = {-1,1} so we don't have to check if it == 0
         int j = jacobi(a, p);
-        biguint_pow(&a, exponent);
+        biguint_pow_mod(&a, p, exponent);
         biguint_mod(a, p, &rem);
 
-        if (rem.limbs[0] != (uint64_t)j)
-            return 0;
+        // (p - 1) mod p = -1 mod p
+        if (biguint_cmp(rem, p_minus_one) == 0 && j == -1) {
+            continue;
+        }
+
+        if (biguint_cmp(rem, num) == 0 && j == 1) {
+            continue;
+        }
+
+        return 0;
     }
 
-    biguint_free(&a, &rem, &exponent, &num);
+    biguint_free(&a, &rem, &exponent, &num, &p_minus_one);
 
     return 1;
-};
+}
 
 /**
  * Jacobi symbol is a generalization over the legendre symbol function.
- * It is defined as the product of the legendre symbol of a over each prime factor
+ * It is defined as the product of the legendre symbol over each prime factor
  *    jacobi(a, n) = legendre(a, p_1)^p_1_k * legendre(a, p_2)^p_2_k * ... * legendre(a, p_n2)^p_n_k
  * Where p_1^p_1_k * p_2^p_2_k * ... * p_n^p_n_k = a
  *
@@ -94,10 +109,10 @@ int biguint_is_prime_solovay_strassen(BigUint p) {
  * It states that given an integer a and an odd integer n, yields:
  * - 0: if a = 0 (mod n)
  * - 1: if a != 0 (mod n) and a is the solution to the quadratic residue of some x mod p
- * - - 1: if a != 0 (mod n) and there isn't a quadratic residue of some x mod p such that a is the solution
+ * - -1: if a != 0 (mod n) and there isn't a quadratic residue of some x mod p such that a is the solution
  *
  *
- * To calculelate it we are recursively doing:
+ * To calculate it we are recursively doing:
  * jacobi(a, n) = {
  *      if a = 1 => 1
  *      if a is even => jacobi(a/2, n) * (-1)^(n^2 - 1) / 8
@@ -152,7 +167,6 @@ int jacobi(BigUint a, BigUint n) {
         biguint_div(a, num, &next);
 
         result = calc * jacobi(next, n);
-
     } else {
         biguint_cpy(&exponent, a);
         biguint_sub(&exponent, num);
