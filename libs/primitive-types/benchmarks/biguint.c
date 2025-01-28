71a719cb39b88020d4099da8309e075880c10f5b
Marcos Nicolau <marcosnicolau@lambdaclass.com>
2025-01-28 19:08:56 -0300

    feat: biguint operation benchmarks

--- libs/primitive-types/benchmarks/biguint.c
+++ libs/primitive-types/benchmarks/biguint.c
@@ -2,16 +2,65 @@
 #include <primitive-types/biguint.h>
 #include <utils/benchmark.h>
 
-void benchmark_divmod() {
-    BigUint a = biguint_new(4);
-    BigUint b = biguint_new(4);
+void benchmark_add() {
+    BigUint a = biguint_new(16);
+    BigUint b = biguint_new(16);
     biguint_random(&a);
     biguint_random(&b);
-    biguint_divmod(a, b, &a, &b);
+    biguint_add(&a, b);
+}
+
+void benchmark_sub() {
+    BigUint a = biguint_new(16);
+    BigUint b = biguint_new(16);
+    biguint_random(&a);
+    biguint_random(&b);
+    biguint_sub(&a, b);
+}
+
+void benchmark_divmod() {
+    BigUint a = biguint_new(16);
+    BigUint b = biguint_new(16);
+    BigUint c = biguint_new(16);
+    BigUint d = biguint_new(16);
+    biguint_random(&a);
+    biguint_random(&b);
+    biguint_divmod(a, b, &c, &d);
+}
+
+void benchmark_mul() {
+    BigUint a = biguint_new(16);
+    BigUint b = biguint_new(16);
+    biguint_random(&a);
+    biguint_random(&b);
+    biguint_mul(&a, b);
+}
+
+void benchmark_pow() {
+    BigUint a = biguint_new(16);
+    BigUint b = biguint_new(16);
+    biguint_random(&a);
+    biguint_random(&b);
+    biguint_pow(&a, b);
+}
+
+void benchmark_pow_mod() {
+    BigUint a = biguint_new(16);
+    BigUint b = biguint_new(16);
+    BigUint m = biguint_new(16);
+    biguint_random(&a);
+    biguint_random(&b);
+    biguint_random(&m);
+    biguint_pow_mod(&a, b, m);
 }
 
 int main() {
     BEGIN_BENCHMARK();
+    benchmark(benchmark_add, 1000000);
+    benchmark(benchmark_sub, 1000000);
     benchmark(benchmark_divmod, 1000000);
+    benchmark(benchmark_mul, 1000000);
+    benchmark(benchmark_pow, 1000);
+    benchmark(benchmark_pow_mod, 10);
     END_BENCHMARK();
 }
