5f59d3d2695fad2ceda9f60734c0513edce93fa0
Marcos Nicolau <marcosnicolau@lambdaclass.com>
2025-02-04 22:45:05 -0300

    test: verify rsa premise in key generation

--- libs/digital-signature/tests/rsa.c
+++ libs/digital-signature/tests/rsa.c
@@ -1,13 +1,25 @@
 #include <digital-signature/rsa.h>
+#include <math/random.h>
 #include <utils/test.h>
 
 void test_key_generation() {
-    RSAKeyPair key_pair = rsa_key_pair_new(512);
+    const int limbs_size = 8;
+    const int size_in_bits = 8 * 64;
+    RSAKeyPair key_pair = rsa_key_pair_new(size_in_bits);
     rsa_gen_key_pair(&key_pair);
 
-    biguint_println(key_pair.pub.n);
-    biguint_println(key_pair.pub.e);
-    biguint_println(key_pair.priv.d);
+    // verify rsa premise: (m^e)^d = m (mod n)
+    BigUint msg = biguint_new(limbs_size);
+    biguint_random(&msg);
+
+    BigUint left_side = biguint_new(limbs_size);
+    biguint_pow_mod(msg, key_pair.pub.e, key_pair.pub.n, &left_side);
+    biguint_pow_mod(left_side, key_pair.priv.d, key_pair.pub.n, &left_side);
+
+    BigUint right_side = biguint_new(limbs_size);
+    biguint_mod(msg, key_pair.pub.n, &right_side);
+
+    assert_that(biguint_cmp(left_side, right_side) == 0);
 }
 
 int main() {
