490ee4a92524d8859572e8bd6776828e8c86bf89
nicolau <marcosnicolau2003@gmail.com>
2025-02-08 21:59:06 -0300

    feat: encrypt/decrypt functions

--- libs/digital-signature/include/rsa.h
+++ libs/digital-signature/include/rsa.h
@@ -3,7 +3,9 @@
 
 #include <math/arithmetics.h>
 #include <math/primes.h>
+#include <math/random.h>
 #include <primitive-types/biguint.h>
+#include <utils/types.h>
 
 typedef struct {
     BigUint n;
@@ -20,6 +22,15 @@ typedef struct {
     unsigned int bit_size;
 } RSAKeyPair;
 
+typedef enum {
+    RSA_MessageTooLong,
+    RSA_MessageTooShort,
+    RSA_InvalidEncodedMessage,
+} RSAError;
+
+DEFINE_RESULT(uint8_t *, RSAError, EncryptResult)
+DEFINE_RESULT(uint8_t *, RSAError, DecryptResult)
+
 #define rsa_key_pair_new(BIT_SIZE)                                                                                     \
     (RSAKeyPair) {                                                                                                     \
         .bit_size = BIT_SIZE, .pub = {.e = biguint_new(BIT_SIZE / 64), .n = biguint_new(BIT_SIZE / 64)}, .priv = {     \
@@ -28,8 +39,8 @@ typedef struct {
     }
 
 void rsa_gen_key_pair(RSAKeyPair *key_pair);
-void rsa_encrypt_msg_PKCS1v15(uint8_t *msg, RSAPrivateKey priv, uint8_t *buffer);
-void rsa_decrypt_msg_PKCS1v15(RSAPublicKey *pub, uint8_t *msg, uint8_t *buffer);
+EncryptResult rsa_encrypt_msg_PKCS1v15(uint8_t *msg, int msg_size, RSAPublicKey pub);
+DecryptResult rsa_decrypt_msg_PKCS1v15(RSAKeyPair key_pair, uint8_t *cipher, int cipher_size);
 void rsa_sign_PKCS1v15(RSAPrivateKey *priv, void *msg, uint8_t *buffer);
 /**
  * @returns
