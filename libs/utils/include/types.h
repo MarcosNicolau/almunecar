ec3b6af8604b8ce8db46269e213589bf555b3480
Marcos Nicolau <marcosnicolau@lambdaclass.com>
2025-02-23 01:20:15 -0300

    feat: uint8array type

--- libs/utils/include/types.h
+++ libs/utils/include/types.h
@@ -1,11 +1,13 @@
 #ifndef TYPES_H
 #define TYPES_H
 
+#include <stdint.h>
+
 // Result like return type like rust
 // https://gist.github.com/f0rki/9b2c2b73d46ccdad2b39ab79b3a5517f
 #define DEFINE_RESULT(T, E, NAME)                                                                                      \
     typedef struct {                                                                                                   \
-        int success : 1;                                                                                               \
+        int success;                                                                                                   \
         union {                                                                                                        \
             T result;                                                                                                  \
             E error;                                                                                                   \
@@ -17,5 +19,10 @@
 
 #define Ok(NAME, R)                                                                                                    \
     (NAME) { .success = 1, .result = R }
+
+typedef struct {
+    uint8_t *array;
+    int size;
+} UInt8Array;
 
 #endif
