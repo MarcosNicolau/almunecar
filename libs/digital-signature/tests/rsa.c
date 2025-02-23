67767cb8b8fdf1375896a23b7242789c13b2a89f
Marcos Nicolau <marcosnicolau@lambdaclass.com>
2025-02-23 01:23:40 -0300

    test: rsa encrypt/decrypt

--- libs/digital-signature/tests/rsa.c
+++ libs/digital-signature/tests/rsa.c
@@ -22,9 +22,31 @@ void test_key_generation() {
     assert_that(biguint_cmp(left_side, right_side) == 0);
 }
 
+void test_encrypt_decrypt_msg() {
+    RSAKeyPair key_pair = rsa_key_pair_new(512);
+    rsa_gen_key_pair(&key_pair);
+
+    char *msg = "Hello this is my secret message!";
+    UInt8Array msg_bytes = {.array = (uint8_t *)msg, .size = strlen(msg)};
+
+    UInt8Array cipher = {};
+    EncryptResult res = rsa_encrypt_msg_PKCS1v15(msg_bytes, key_pair.pub, &cipher);
+    assert_that(res.success == 1);
+
+    UInt8Array decrypted_msg = {};
+    DecryptResult decrypt_result = rsa_decrypt_msg_PKCS1v15(key_pair, cipher, &decrypted_msg);
+    assert_that(decrypt_result.success == 1);
+
+    assert_that(strcmp((char *)decrypted_msg.array, msg) == 0);
+
+    free(cipher.array);
+    free(decrypted_msg.array);
+}
+
 int main() {
     BEGIN_TEST()
     test(test_key_generation);
+    test(test_encrypt_decrypt_msg);
     END_TEST()
 
     return 0;
