44628ae1b59d4128ac141bfa7acc0da31f5ca2b1
nicolau <marcosnicolau2003@gmail.com>
2025-02-25 00:46:38 -0300

    chore: document sign/verify functions

--- libs/digital-signature/include/rsa.h
+++ libs/digital-signature/include/rsa.h
@@ -22,6 +22,13 @@ typedef struct {
     unsigned int bit_size;
 } RSAKeyPair;
 
+/**
+ * List of RSA-supported and recommended hash algorithms.
+ *
+ * Note: This enumeration includes widely used hash functions for RSA signatures.
+ * However, this library **only supports RSA_HASH_SHA256** for signing and verification.
+ * Attempting to use any other hash will result in RSA_HashNotSupported.
+ */
 typedef enum { RSA_HASH_MD2, RSA_HASH_MD5, RSA_HASH_SHA1, RSA_HASH_SHA256, RSA_HASH_SHA384, RSA_HASH_SHA512 } RSAHashes;
 
 typedef enum {
@@ -88,13 +95,34 @@ RSAEncryptResult rsa_encrypt_msg_PKCS1v15(UInt8Array msg, RSAPublicKey pub, UInt
  */
 RSADecryptResult rsa_decrypt_msg_PKCS1v15(RSAKeyPair key_pair, UInt8Array cipher, UInt8Array *msg);
 
+/**
+ * Signs a message using the RSA PKCS1 v1.5 padding scheme.
+ *
+ * If `signature->array` is NULL or `signature->size` is too small, the buffer will be allocated or reallocated as
+ * needed. The caller is responsible for freeing the allocated memory.
+ *
+ * WARNING: Only RSA_HASH_SHA256 is supported. Using any other hash algorithm will result in RSA_HasNotSupported.
+ *
+ * @param msg       The message to sign.
+ * @param key_pair  The RSA key pair containing the private key.
+ * @param hash      The hash algorithm to use for signing. **Only RSA_HASH_SHA256 is supported.**
+ * @param signature A pointer to a UInt8Array where the signature will be stored.
+ *                  If NULL, a new buffer will be allocated.
+ * @return          RSASignResult containing the signature or an error.
+ */
 RSASignResult rsa_sign_PKCS1v15(UInt8Array msg, RSAKeyPair key_pair, RSAHashes hash, UInt8Array *signature);
 
 /**
- * @returns
- *  - 1: valid signature
+ * Verifies an RSA signature using the PKCS1 v1.5 padding scheme.
  *
- *  - 0: invalid signature
+ * This function checks whether the given signature is valid for the provided message and public key.
+ *
+ * WARNING: Only RSA_HASH_SHA256 is supported. Using any other hash algorithm will result in RSA_HasNotSupported.
+ *
+ * @param msg       The original message.
+ * @param signature The signature to verify.
+ * @param pub       The RSA public key.
+ * @return          RSAVerificationResult indicating whether the signature is valid or an error.
  */
 RSAVerificationResult rsa_verify_signature_PKCS1v15(UInt8Array msg, UInt8Array signature, RSAPublicKey pub);
 
